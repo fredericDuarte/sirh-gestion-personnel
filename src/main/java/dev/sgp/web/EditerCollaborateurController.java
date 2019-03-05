ca6188dc19c114ac03b9fc5b2d5882bf40809dd6
Frederic DUARTE <geforst@gmail.com>
2019-03-05 16:25:33 +0100

    TP JavaEE Servlet add editer

--- src/main/java/dev/sgp/web/EditerCollaborateurController.java
+++ src/main/java/dev/sgp/web/EditerCollaborateurController.java
@@ -1,6 +1,11 @@
 package dev.sgp.web;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +17,8 @@ public class EditerCollaborateurController extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private HashMap<String, String> paramUrl;
+	private boolean isfull = true;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -20,7 +27,6 @@ public class EditerCollaborateurController extends HttpServlet {
 		String matricule = req.getParameter("matricule");
 
 		if (matricule == null) {
-			// 500 //throw new ServletException("Un matricule est attendu");
 			resp.sendError(400, "Un matricule est attendu");
 		} else {
 			resp.setContentType("text/html");
@@ -34,26 +40,49 @@ public class EditerCollaborateurController extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		String matricule = req.getParameter("matricule");
-		String titre = req.getParameter("titre");
-		String nom = req.getParameter("nom");
-		String prenom = req.getParameter("prenom");
+		paramUrl = new HashMap<String, String>();
+
+		paramUrl.put("matricule", req.getParameter("matricule"));
+		paramUrl.put("titre", req.getParameter("titre"));
+		paramUrl.put("nom", req.getParameter("nom"));
+		paramUrl.put("prenom", req.getParameter("prenom"));
 
 		resp.setContentType("text/html");
+		isfullParamter(paramUrl);
+		if (!isfull) {
+			resp.sendError(400, "Les paramètres suivants sont incorrects." + isfullParamter(paramUrl)
 
-		if ((matricule == null) || (titre == null) || (nom == null) || (prenom == null)) {
-
-			// 500 //throw new ServletException("Les paramètres suivants sont incorrects.");
-			resp.sendError(400, "Les paramètres suivants sont incorrects.");
+			);
 
 		} else {
 
-			// code HTML ecrit dans le corps de la reponse
-			resp.getWriter()
-					.write("<h1>Creation d'un collaborateurs</h1>" + "<ul>" + "<li>Matricule=" + matricule + "</li>"
-							+ "<li>Titre=" + titre + "</li>" + "<li>Nom=" + nom + "</li>" + "<li>Prénom=" + prenom
-							+ "</li>" + "</ul>");
+			resp.getWriter().write(forfullParamter(paramUrl));
 		}
+
+	}
+
+	protected String isfullParamter(HashMap<String, String> param) {
+		String chaine = "";
+		for (Entry<String, String> par : param.entrySet()) {
+			if (par.getValue() == null) {
+				chaine += par.getKey() + " , ";
+				this.isfull = false;
+			}
+		}
+
+		return chaine;
+
+	}
+
+	protected String forfullParamter(HashMap<String, String> param) {
+		String chaine = "Creation d’un collaborateur /n";
+		for (Entry<String, String> par : param.entrySet()) {
+
+			chaine += par.getKey() + " = " + par.getValue() + " , ";
+
+		}
+
+		return chaine;
 
 	}
 
