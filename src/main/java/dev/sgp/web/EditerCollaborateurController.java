dfcfe8005d7e4cd77698e6a431fe9c854b1fc723
Frederic DUARTE <geforst@gmail.com>
2019-03-05 15:05:32 +0100

    TP JavaEE Servlet

---
+++ src/main/java/dev/sgp/web/EditerCollaborateurController.java
@@ -0,0 +1,63 @@
+package dev.sgp.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EditerCollaborateurController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		// recupere la valeur d'un parametre dont le nom est matricule
+		String matricule = req.getParameter("matricule");
+
+		if (matricule == null) {
+			throw new ServletException("Un matricule est attendu");
+		} else {
+			resp.setContentType("text/html");
+
+			// code HTML ecrit dans le corps de la reponse
+			resp.getWriter().write(
+					"<h1>Edition des collaborateurs</h1>" + "<ul>" + "<li>Matricule=" + matricule + "</li>" + "</ul>");
+		}
+	}
+	
+	
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String matricule = req.getParameter("matricule");
+		String titre = req.getParameter("titre");
+		String nom = req.getParameter("nom");
+		String prenom = req.getParameter("prenom");
+		
+		if( (matricule == null ) || ( titre == null ) || ( nom == null )  || ( prenom == null ) )  {
+			throw new ServletException("Les paramètres suivants sont incorrects.");
+		} else {
+			resp.setContentType("text/html");
+
+			// code HTML ecrit dans le corps de la reponse
+			resp.getWriter().write(
+					"<h1>Creation d'un collaborateurs</h1>" 
+							+ "<ul>" 
+							+ "<li>Matricule=" + matricule + "</li>" 
+							+ "<li>Titre=" + titre + "</li>" 
+							+ "<li>Nom=" + nom + "</li>" 
+							+ "<li>Prénom=" + prenom + "</li>" 
+							+ "</ul>");
+		}
+		
+	}
+	
+	
+
+}
